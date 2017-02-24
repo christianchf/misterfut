1b5c695ad627cdece2671014e668b87f92b5b527
Christian Hidalgo <christianhf.chf@gmail.com>
2017-02-24 15:42:13 +0100

    Creación de la tabla de usuarios

---
+++ db/misterfut.sql
@@ -0,0 +1,11 @@
+drop table if exists usuarios cascade;
+
+create table usuarios (
+    id         bigserial    constraint pk_usuarios primary key,
+    nombre     varchar(15)  not null constraint uq_usuarios_nombre unique,
+    password   varchar(60)  not null,
+    email      varchar(255) not null,
+    token      varchar(32),
+    --activacion varchar(32),
+    created_at timestamptz  default current_timestamp
+);
