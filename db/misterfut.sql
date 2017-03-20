bdc07144011f9789c23549b112cac09a17af853c
Christian Hidalgo <christianhf.chf@gmail.com>
2017-03-20 18:49:30 +0100

    Añade a la base de datos equipos, posiciones y jugadores

--- db/misterfut.sql
+++ db/misterfut.sql
@@ -18,9 +18,54 @@ insert into usuarios(nombre, email, password)
 
 drop table if exists session;
 
-create table session
-(
+create table session (
     id char(40) not null primary key,
     expire integer,
     data BYTEA
+);
+
+drop table if exists equipos cascade;
+
+create table equipos (
+    id                 bigserial    constraint pk_equipos primary key,
+    nombre             varchar(100) not null constraint uq_equipos_nombre unique,
+    partidos_ganados   numeric(3)   default 0,
+    partidos_empatados numeric(3)   default 0,
+    partidos_perdidos  numeric(3)   default 0,
+    goles_a_favor      numeric(3)   default 0,
+    goles_en_contra    numeric(3)   default 0,
+    id_usuario         bigint       not null constraint fk_equipos_usuarios
+                                             references usuarios (id)
+                                             on delete cascade on update cascade
+);
+
+drop table if exists posiciones cascade;
+
+create table posiciones (
+    id       bigserial    constraint pk_posiciones primary key,
+    posicion varchar(100) not null
+);
+
+insert into posiciones(posicion)
+    values  ('POR'), ('CAD'), ('LD'), ('DFC'), ('LI'), ('CAI'), ('MCD'), ('MD'),
+            ('MC'), ('MI'), ('MCO'), ('SDD'), ('SD'), ('SDI'), ('ED'), ('DC'),
+            ('EI');
+
+drop table if exists jugadores cascade;
+
+create table jugadores (
+    id               bigserial    constraint pk_jugadores primary key,
+    nombre           varchar(100) not null,
+    fecha_nac        date         not null,
+    dorsal           numeric(2)   not null,
+    partidos_jugados numeric(3)   default 0,
+    goles_marcados   numeric(3)   default 0,
+    goles_encajados  numeric(3)   default 0,
+    asistencias      numeric(3)   default 0,
+    id_equipo        bigint       not null constraint fk_jugadores_equipos
+                                           references equipos (id)
+                                           on delete cascade on update cascade,
+    id_posicion      bigint       not null constraint fk_jugadores_posiciones
+                                           references posiciones (id)
+                                           on delete no action on update cascade
 );
