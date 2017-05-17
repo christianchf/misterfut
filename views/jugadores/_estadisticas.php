a660b5ebadba001e2cfc08ef705e8faf3d382f11
Christian Hidalgo <christianhf.chf@gmail.com>
2017-05-17 19:17:26 +0200

    Extrae código js de las vistas

--- views/jugadores/_estadisticas.php
+++ views/jugadores/_estadisticas.php
@@ -1,70 +1,22 @@
 <?php
 use app\assets\AppAsset;
+use yii\web\View;
 use yii\helpers\Url;
 
 AppAsset::register($this);
 
-$pos = $model->id_posicion;
-$url = Url::to(['/jugadores/actualizar', 'id' => Yii::$app->request->get('id')]);
+$posJugador = $model->id_posicion;
+$urlJugador = Url::to(['/jugadores/actualizar', 'id' => Yii::$app->request->get('id')]);
 
 $js = <<<EOT
-    var btns = $('.btnActualizar');
-
-    $(document).on('ready', function () {
-        generarBtnsPortero();
-        $.ajax({
-            url: "$url",
-            method: 'POST',
-            success: function(data, textStatus, Xhr) {
-                var datos = JSON.parse(data);
-                mostrarDatos(datos);
-            }
-        });
-    });
-
-    btns.on('click', ajaxBotones);
-
-    function mostrarDatos(datos) {
-        $('#jugados').text(datos['jugados']);
-        $('#golesMarcados').text(datos['golesMarcados']);
-        $('#golesEncajados').text(datos['golesEncajados']);
-        $('#asistencias').text(datos['asistencias']);
-        $('#golesPartido').text(datos['golesPartido'] + ' %');
-    }
-
-    function ajaxBotones() {
-        var idBtn = $(this).attr('id');
-        $.ajax({
-            url: "$url",
-            method: 'POST',
-            data: JSON.stringify({'idBtn': idBtn}),
-            success: function(data, textStatus, Xhr) {
-                var datos = JSON.parse(data);
-                mostrarDatos(datos);
-            }
-        });
-    }
-
-    function generarBtnsPortero() {
-        var cabGolesEncaj = '<th>Goles encajados</th>';
-        var sumGolesEncaj = '<td><button id="suma3" class="btn btn-xs btn-info btnMas btnActualizar">' +
-            '<span class="glyphicon glyphicon-plus"></span></button></td>';
-        var golesEncaj = '<td id="golesEncajados"></td>';
-        var restGolesEncaj = '<td><button id="resta3" class="btn btn-xs btn-warning btnMenos btnActualizar">' +
-        '<span class="glyphicon glyphicon-minus"></span></button></td>';
-
-        if ($pos == 1) {
-            $("#cabecera").append(cabGolesEncaj);
-            $("#btnsSuma").append(sumGolesEncaj);
-            $("#datosJugador").append(golesEncaj);
-            $("#btnsResta").append(restGolesEncaj);
-            $("#suma3").on('click', ajaxBotones);
-            $("#resta3").on('click', ajaxBotones);
-        }
-    }
-
+    var urlJugador = "$urlJugador";
+    var posJugador = "$posJugador";
 EOT;
-$this->registerJs($js);
+$this->registerJs($js, View::POS_END);
+$this->registerJsFile(
+    '/js/ajaxJugadores.js',
+    ['depends' => [\yii\web\JqueryAsset::className()]]
+);
 
 ?>
 
---
+++ web/js/traspaso.js
@@ -0,0 +1,23 @@
+// Ajax para realizar el traspaso másivo de todos los jugadores de un equipo del
+// usuario a otro del mismo usuario
+
+var padre = window.opener;
+$('#traspaso').on('click', function(){
+    var origen = $('#jugadorsearch-nombre').val();
+    var destino = idEquipoTraspasar;
+    var equipos = JSON.stringify({'origen': origen, 'destino': destino});
+
+    $.ajax({
+        url: urlTraspasar,
+        contentType: 'application/json',
+        method: 'POST',
+        data: equipos,
+        success: function(data, textStatus, Xhr) {
+            $('#indice').load(urlDestinoTraspasar + ' #w1-container');
+        }
+    });
+    setTimeout(function(){
+        padre.location.href=urlDestinoTraspasar;
+        close();
+    }, 1500);
+});
