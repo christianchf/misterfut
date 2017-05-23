380614eb62b05ef8c488cb56efde3abd9aef3fb5
Christian Hidalgo <christianhf.chf@gmail.com>
2017-05-23 18:41:44 +0200

    Añade plugin slick

---
+++ assets/IndexAsset.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+class IndexAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+        '/js/slick/slick.css',
+        '/js/slick/slick-theme.css',
+    ];
+    public $js = [
+        '/js/slick/slick.min.js',
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+        'yii\web\JqueryAsset',
+    ];
+}
--- views/site/index.php
+++ views/site/index.php
@@ -1,16 +1,31 @@
 <?php
+use app\assets\IndexAsset;
+use yii\web\View;
 
 /* @var $this yii\web\View */
 
+IndexAsset::register($this);
+
 $this->title = 'MisterFut';
+$js = <<<EOT
+    $(document).ready(function(){
+        $('.carrusel-slick').slick({
+            dots: true,
+            infinite: true,
+            autoplay: true,
+            autoplaySpeed: 2000,
+            pauseOnHover: false
+        });
+    });
+EOT;
+$this->registerJs($js, View::POS_END);
 ?>
 <div class="site-index">
-
-    <div class="jumbotron">
-        <h1>MisterFut</h1>
-    </div>
-
     <div class="body-content">
-
+        <br><br><br>
+        <div class="carrusel-slick">
+            <div><img src="/images/pelota-campo.jpg" alt="Pelota en campo de fútbol"></div>
+            <div><img src="/images/pizarra-tactica.jpg" alt="Pizarra de tácticas"></div>
+        </div>
     </div>
 </div>
