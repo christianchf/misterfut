0db8ac434bf6e9edfbe2c206fb0e6ef88a4f508b
Christian Hidalgo <christianhf.chf@gmail.com>
2017-06-09 18:06:57 +0200

    Añade nueva versión de la documentación

---
+++ tests/functional/ModificarEventoCest.php
@@ -0,0 +1,29 @@
+<?php
+
+use app\models\Usuario;
+
+class ModificarEventoCest
+{
+    public function _before(FunctionalTester $I)
+    {
+        $I->amLoggedInAs(Usuario::findOne(1));
+        $I->amOnRoute('eventos/update', ['id' => 3]);
+    }
+    public function abrirCrearEvento(FunctionalTester $I)
+    {
+        $I->see('Modificar Evento: Entrenamiento: Entrenamiento físico', 'h1');
+    }
+    public function enviarFormularioCorrecto(\FunctionalTester $I)
+    {
+        $I->submitForm('#anadirEvento', [
+            'Evento[tipo]' => 'Entrenamiento',
+            'Evento[titulo]' => 'Entrenamiento físico',
+            'Evento[fecha_inicio]' => '2017-06-03',
+            'Evento[hora_inicio]' => '09:00',
+            'Evento[fecha_fin]' => '2017-06-03',
+            'Evento[hora_fin]' => '11:00',
+        ]);
+        $I->dontSeeElement('#anadirEvento');
+        $I->see('Calendario Real Madrid (2016/2017)', 'h1');
+    }
+}
